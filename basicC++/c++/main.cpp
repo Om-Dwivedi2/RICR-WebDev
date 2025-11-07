3f0cbb0436550f128771af053178435c8a223148
Om-Dwivedi2 <dwivediom2005@gmail.com>
2025-11-07 18:10:05 +0530

    commit

--- basicC++/c++/main.cpp
+++ basicC++/c++/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
+
+struct student
+{
+    string name;
+    int age;
+    int year;  
+};
+
+
 int main() {
-    int num=1;
-    while (num<12)
-    {
-        cout<< num++ << " ";
-        
-    }
+    
+    student om = {"Om" , 20 , 2};
+    student harshit = {"Harshit" , 21 , 4};
+    student Nayan = {"Nayan" , 19 , 3};
+
     
     
 
-    return 0;
+   return 0;
 }
