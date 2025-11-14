777a3b85963df77ce16149d2a552976154dc5848
Om-Dwivedi2 <dwivediom2005@gmail.com>
2025-11-14 20:39:03 +0530

    hello

---
+++ JavaScript/JS Basics/index.js
@@ -0,0 +1,22 @@
+var a = 10;
+
+var a = 1;
+
+let b = 10;
+
+console.log(a, b);
+
+let ar = ["abc", 123, 43.43];
+console.log(ar);
+console.warn("this is warning");
+console.error("ye error hai");
+
+// alert("this is alert");
+
+if (confirm("Are you Sure")) {
+    console.log("thanks")
+
+}else{
+    console.error("yoyo")
+}
+
