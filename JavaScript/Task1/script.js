1b355f1a185aef43d4d7dadfc63135500c393a8b
Om-Dwivedi2 <dwivediom2005@gmail.com>
2025-11-17 20:34:26 +0530

    Task Done

---
+++ JavaScript/Task1/script.js
@@ -0,0 +1,44 @@
+function login() {
+    console.log("Login Button Clicked");
+
+    // const dala hai jisse hamara fetch kiya hua data koi change naa kar sake
+    // niche document 1 universal class hai JS ki jisme html ks code hota hai
+    const om = document.getElementById("LoginEmail").value;
+    const dev = document.getElementById("LoginPassword").value; 
+    console.log("Login:- " + om);
+    console.log("Password:- " + dev);
+    
+    alert("Login Done");
+
+    document.getElementById("LoginEmail").value = "";
+    document.getElementById("LoginPassword").value = "";
+
+
+
+}
+
+function registration(){
+    console.log("Registration Button Clicked");
+
+    const a = document.getElementById("RegistrationName").value;
+    const b = document.getElementById("RegistrationEmail").value;
+    const c = document.getElementById("RegistrationPassword").value;
+    const d = document.getElementById("ConfirmPassword").value;
+
+    console.log("Name:- " + a);
+    console.log("Email:- " + b);
+    console.log("Password:- " + c);
+    console.log("Confirm Password:- " + c);
+
+
+
+
+    alert("Login Done");
+
+    document.getElementById("RegistrationName").value = "";
+    document.getElementById("RegistrationEmail").value = "";
+    document.getElementById("RegistrationPassword").value = "";
+    document.getElementById("ConfirmPassword").value = "";
+
+
+}
