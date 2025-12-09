f22f5b5f154caeaa744b34add8c4b8c7cb51394b
Om-Dwivedi2 <dwivediom2005@gmail.com>
2025-12-09 16:31:00 +0530

    prodject done

--- JavaScript/RoughCopy/script.js
+++ JavaScript/RoughCopy/script.js
@@ -1,25 +1,8 @@
-let om = document.getElementById("me");
+let file = document.getElementById("file");
 
 
 
-    
-    om.addEventListener("click" , () => {
-        console.log("hello");
-        if (om.textContent === "Click Me!") {
-            let b = document.createElement("button");
-            b.classList.add("m-5" , "ram");
-            b.innerText = "Om Dwivedi";
-            om.textContent = "Remove";
-
-            let body = document.querySelector("body");
-            body.appendChild(b);
-        }
-
-        else{
-            document.querySelector("body").removeChild(document.querySelector(".ram")); 
-            om.textContent = "Click Me!";
-        }
-    });
-
-
+function om(){
+    file.click();
+}
 
---
+++ JavaScript/Storage/Local Storage/Basics/script.js
@@ -0,0 +1,6 @@
+function AddData(){
+    const name = document.getElementById("name");
+    const user = document.getElementById("name");
+    const named = document.getElementById("name");
+
+}
