e19faffa8d31f9ba27a9269339cc3918fe08e3ef
Om-Dwivedi2 <dwivediom2005@gmail.com>
2025-12-25 14:47:55 +0530

    React Started

---
+++ React/Demo/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App(){
+  let a = 5;
+  let b = 10;
+
+  console.log(a+b);
+  return(
+    <>
+    <h1 id="head">Hello World</h1>
+    <div>this is my first app</div>
+
+    </>
+  );
+  
+}
+
+export default App;
