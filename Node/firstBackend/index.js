07a46d11f9df3a63fdf9ec5636d476f7ad85197a
Om-Dwivedi2 <dwivediom2005@gmail.com>
2026-01-10 18:29:33 +0530

    abc

--- Node/firstBackend/index.js
+++ Node/firstBackend/index.js
@@ -2,8 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import connectDB from "./src/config/db.js"
+import AuthRouter from "./src/routers/myRouter.js"
 
 const app = express();
+
+app.use("/auth" , AuthRouter);
+
+app.use(express.json())  // frontend se data bheje hai usse readable form me convert kar rahe hai.
+
+
 
 // frontEnd se
 app.get("/", (req, res) => {
@@ -14,4 +22,6 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server Started at Port", port);
+  connectDB();
 });
+ 
---
+++ Node/firstBackend/src/config/db.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=> {
+
+    try {
+    // process.env.MONGO_URI :- chache memory se data la rahe hai  
+       const conn = await mongoose.connect(process.env.MONGO_URI);  // Connecting the MongoDB 
+       
+       console.log(`MongoDB Connected at : ${conn.connection.host} :${conn.connection.port}`);
+       //                                  OR
+       console.log("MongoDB Connected at" ,  conn.connection.host , ":" , conn.connection.port);
+       // in the "" wali line due to {comma} we get an extra space so we used 
+       
+       console.log("Database Name : ", conn.connection.name);
+    
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+        
+    }
+}
+
+export default connectDB;
